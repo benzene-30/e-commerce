cca7cc5a4e63e479458b48ede84d83218a965b30
Benz-19 <kingsleyikenna2019@gmail.com>
2024-09-09 03:41:32 +0300

    header.php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="nav-bar">
         <ul>
-            <li><a href="#"><img src="../assets/images/logo.png" alt="logo" class='logo'></a></li>
+            <li><a href="#"><img src="./assets/images/logo.png" alt="logo" class='logo'></a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">Contact</a></li>
@@ -13,7 +13,7 @@
 
                 <div class="cart-header">
                     <a href="../products/cart.php">
-                        <img src="../assets/images/cart.png" alt="Cart image" class='cart'>
+                        <img src="./assets/images/cart.png" alt="Cart image" class='cart'>
                         <span class="cart-count"><?php echo $cart_count; ?></span>
                     </a>
                 </div>
